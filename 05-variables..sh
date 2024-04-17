4cd331a4ec164636853cb71b0b1417ebcd140ec9
Murali Krishna Vujji <vujjimuralikrishna@gmail.com>
2024-04-17 12:34:54 +0530

    shell-practice

--- 05-variables..sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-PERSON1=$1
-PERSON2=$2
-
-$1 is talking to $2.
---
+++ 05-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#declaring variable
+PERSON1=$1
+PERSON2=$2
+
+#referring variable
+echo $PERSON1 is talking to $PERSON2.
