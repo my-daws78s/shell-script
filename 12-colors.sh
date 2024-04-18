de8b53e56d868b9ad0ef3c0ed17f7eb0c5887c54
Murali Krishna Vujji <vujjimuralikrishna@gmail.com>
2024-04-18 19:24:10 +0530

    functions practice

---
+++ 12-colors.sh
@@ -0,0 +1,35 @@
+#!bin/bash
+
+#Adding colors to output messages:
+
+CHECKUID=$(id -u)
+if [ $CHECKUID -ne 0 ]
+then
+    echo "It is not a root user."
+    exit 1
+else
+    echo "I am a root user."
+fi
+
+DATE=$(date +%F-%H-%M-%S)
+FILENAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$FILENAME-$DATE.log
+
+R=\e[31m
+G=\e[32m
+N=\e[0m
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2..... $R Failure! $N"
+    else
+        echo -e "$2..... $G Success! $N"
+    fi
+}
+
+dnf install git -y &>> $LOGFILE
+VALIDATE $? "Installation of GIT Is ::"
+
+dnf install dockerrr -y &>> $LOGFILE
+VALIDATE $? "Installation of Docker Is ::"
