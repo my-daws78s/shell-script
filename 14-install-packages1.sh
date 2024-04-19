576c94fab9b20b0b45829490feca7fc2def12ab9
Murali Krishna Vujji <vujjimuralikrishna@gmail.com>
2024-04-19 06:54:49 +0530

    shell-practice

--- 14-install-packages1.sh
+++ 14-install-packages1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-USERID $(id -u)
+USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
@@ -23,4 +23,5 @@ do
         echo "Package $i is already installed.... SKIPPING"
     else
         echo "Package $i is to be installed."
+    fi
 done
