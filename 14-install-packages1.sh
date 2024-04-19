545d3b436f4cd41aa959a532b0fffb781d200371
Murali Krishna Vujji <vujjimuralikrishna@gmail.com>
2024-04-19 07:00:06 +0530

    shell-practice

--- 14-install-packages1.sh
+++ 14-install-packages1.sh
@@ -10,18 +10,22 @@ else
     echo "You are a root user."
 fi
 
-TIMESTAMP="date +%F-%H-%M-%S"
+TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 for i in $@
 do
     echo "Package to install: $i"
-    dnf installed mysql $>>$LOGFILE
+    dnf installed $i &>>$LOGFILE
     if [ $? -eq 0 ]
     then
-        echo "Package $i is already installed.... SKIPPING"
+        echo -e "Package $i is already installed.... $Y SKIPPING $N"
     else
-        echo "Package $i is to be installed."
+        echo -e "$R Package $i is not installed yet.... $N"
     fi
 done
