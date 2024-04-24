e5525825e4c74df7a6c5782005c98697c40dbeae
Murali Krishna Vujji <vujjimuralikrishna@gmail.com>
2024-04-24 13:57:37 +0530

    testing 2 ways of calling shell scripts

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -9,3 +9,16 @@ echo "PID of current script is: $$"
 echo "$COURSE"
 echo "PID of current script is: $$"
 
+# Output for above is as follows:
+
+# I am in the current script
+# PID of current script is: 1653
+# I am in the other script now.
+# 1654 is the PID of other script.
+# I am in the current script
+# PID of current script is: 1653
+
+echo "Testing source way of calling shell script ......."
+source ./16-other-script.sh
+echo "$COURSE"
+echo "PID of current script is: $$"
