3dad9f7171be1c8313c7e693f51493f310a68db4
Murali Krishna Vujji <vujjimuralikrishna@gmail.com>
2024-04-25 18:55:48 +0530

    disk usage script testing

--- 18-delete-old-logs1.sh
+++ 18-delete-old-logs1.sh
@@ -21,7 +21,7 @@ echo "Files to delete are listed here: $FILES"
 # done <<< $FILES
 
 #test the below code:
-for i in {$@}
+for i in {FILES[@]}
 do
     echo "Deleting File using for loop: $i" 
     #echo "Deleting File using for loop1: $FILES" 
