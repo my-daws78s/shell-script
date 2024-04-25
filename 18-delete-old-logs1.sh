3b3ab1abe046052044767f0bad5b514e0c830585
Murali Krishna Vujji <vujjimuralikrishna@gmail.com>
2024-04-25 19:25:38 +0530

    disk usage script testing

--- 18-delete-old-logs1.sh
+++ 18-delete-old-logs1.sh
@@ -14,15 +14,21 @@ fi
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime -5)
 echo "Files to delete are listed here: $FILES"
 
+# Below while loop perfectly working:
 # while IFS= read -r line
 # do
 #     echo "Deleting File: $line"
 #     #rm -fr $line
 # done <<< $FILES
 
-#test the below code: seems while is best for reading each line... dont find a way to handle in for loop...
+#tested the below code and it works as well, I was trying with {$FILES} and that was causing 
+#issue in deleting files:
 for i in $FILES
 do
     echo "Deleting File using for loop: $i" 
     rm -fr $i
 done
+
+# Tested via crontab as well:
+# */2 * * * * sh /home/centos/shell-script/18-delete-old-logs1.sh /tmp/app-logs/
+# tail -f /var/log/cron
