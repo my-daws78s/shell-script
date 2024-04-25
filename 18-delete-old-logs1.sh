808e5f93ed8e2e1c377eaa1ef4f0e2123a3bcb18
Murali Krishna Vujji <vujjimuralikrishna@gmail.com>
2024-04-25 19:10:05 +0530

    disk usage script testing

--- 18-delete-old-logs1.sh
+++ 18-delete-old-logs1.sh
@@ -14,16 +14,16 @@ fi
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +5)
 echo "Files to delete are listed here: $FILES"
 
-while IFS= read -r line
-do
-    echo "Deleting File: $line"
-    #rm -fr $line
-done <<< $FILES
+# while IFS= read -r line
+# do
+#     echo "Deleting File: $line"
+#     #rm -fr $line
+# done <<< $FILES
 
 #test the below code: seems while is best for reading each line... dont find a way to handle in for loop...
-# for i in {FILES[@]}
-# do
-#     echo "Deleting File using for loop: $i" 
-#     #echo "Deleting File using for loop1: $FILES" 
-#     #rm -fr $i
-# done
+for i in {$FILES}
+do
+    echo -e "Deleting File using for loop: $i" 
+    #echo "Deleting File using for loop1: $FILES" 
+    #rm -fr $i
+done
